be0272a0110e768a1b4d1e214d50c71d827915d8
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2023-12-07 22:48:32 +0200

    start client service

---
+++ client/pages/banana.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const Banana = () => {
+  return <h1>banana page</h1>;
+};
+
+export default Banana;
---
+++ client/pages/index.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const LandingPage = () => {
+  return <h1>landing page</h1>;
+};
+
+export default LandingPage;
