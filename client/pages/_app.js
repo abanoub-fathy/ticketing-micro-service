111dc1769cbc2ee01607846f58ce153ee29ec9aa
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2023-12-09 04:27:53 +0200

    create App component and use bootstrap css

---
+++ client/pages/_app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
