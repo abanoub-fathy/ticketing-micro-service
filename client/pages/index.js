01c9e9cee6e01bb5f3c8edf2bdda0ca0c466a300
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2023-12-18 05:02:43 +0200

    add a heading for inform if the user signed in or not in the landingPage

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log("currentUser =", currentUser);
-
-  return <h1>LandingPage</h1>;
+  return <h1>You are {!currentUser && "Not"} Signed in</h1>;
 };
 
 // getInitialProps is used for server side rendering
