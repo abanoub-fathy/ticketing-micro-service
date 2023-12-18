07d94570a74c4c3b793eec9fa9e108b70854b25c
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2023-12-18 04:55:46 +0200

    refactor the code inside getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,20 +1,27 @@
 import React from "react";
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
+  console.log("currentUser =", currentUser);
+
   return <h1>LandingPage</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  // we can't use useRequest here because inside getInitialProps we
-  // can't use hooks
+// getInitialProps is used for server side rendering
+// it can be run on the server and also in special cases on the client
+// we need to handle the two cases
+//
+// we can't use useRequest to send request here because
+//  inside getInitialProps we can't use hooks
+LandingPage.getInitialProps = async (context) => {
+  // build client according to the context
+  const client = buildClient(context);
 
   try {
-    const response = await axios.get("/api/users/current");
-    return response.data;
+    const { data } = await client.get("/api/users/current");
+    return data;
   } catch (err) {
-    console.log(err);
+    console.log("Error while running getInitialProps =", err);
     return { currentUser: null };
   }
 };
