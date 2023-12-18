2c554411e7fa87690335eeb069fcfa3789e52fa6
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2023-12-18 05:24:53 +0200

    add header placehlder in the App component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,7 +2,12 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <h1>Header ...</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default App;
--- client/pages/banana.js
+++
@@ -1,7 +0,0 @@
-import react from "react";
-
-const Banana = () => {
-  return <h1>banana page</h1>;
-};
-
-export default Banana;
