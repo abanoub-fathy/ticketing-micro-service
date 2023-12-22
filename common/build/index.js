dfca87aaa35b4a15284b195c9da7b6494cacf9f4
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2023-12-22 20:16:37 +0200

    setup the common shared library

---
+++ common/build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const newColor = {
+    red: 255,
+    green: 255,
+    blue: 255,
+};
+console.log(newColor);
---
+++ common/src/index.ts
@@ -0,0 +1,13 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const newColor: Color = {
+  red: 255,
+  green: 255,
+  blue: 255,
+};
+
+console.log(newColor);
