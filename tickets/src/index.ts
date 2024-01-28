0d31c3fada749fe8d21b88f7020e880f779abbed
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2024-01-28 21:24:01 +0200

    use env vars for the tickets service nats connection vars

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,11 +12,27 @@ const start = async () => {
     throw new Error("MONGO_URI must be defined");
   }
 
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("📅 Database is connected successfully!");
 
-    await stan.connect("ticketing", "sdsdsd", "http://nats-srv:4222");
+    await stan.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     stan.client.on("close", () => {
       console.log("closing down the listener");
       process.exit();
