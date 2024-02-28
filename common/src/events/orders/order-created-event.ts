c225af419661e6e35f48dbe860f982a320c23de0
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2024-02-28 07:15:42 +0200

    updates

--- common/src/events/orders/order-created-event.ts
+++ common/src/events/orders/order-created-event.ts
@@ -5,6 +5,7 @@ export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
+    version: number;
     userId: string;
     status: OrderStatus;
     expiresAt: string;
