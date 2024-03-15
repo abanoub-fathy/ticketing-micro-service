4e8595f3cc2624d2c04e6b1d37c3e3c546fc8b7b
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2024-03-15 23:23:18 +0200

    add order model in payments service

---
+++ payments/models/order.ts
@@ -0,0 +1,62 @@
+import { OrderStatus } from "@ticketiano/common";
+import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+
+interface OrderAttrs {
+  id: string;
+  version: number;
+  userId: string;
+  status: OrderStatus;
+  price: number;
+}
+
+interface OrderDoc extends mongoose.Document {
+  version: number;
+  userId: string;
+  status: OrderStatus;
+  price: number;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      values: Object.values(OrderStatus),
+      required: [true, "status is required"],
+    },
+    userId: {
+      type: String,
+      required: [true, "userId is required"],
+    },
+    price: {
+      type: Number,
+      required: [true, "price is required"],
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = function (attrs: OrderAttrs): OrderDoc {
+  return new Order({
+    _id: attrs.id,
+    ...attrs,
+  });
+};
+
+orderSchema.set("versionKey", "version");
+orderSchema.plugin(updateIfCurrentPlugin);
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
+
+export { Order };
