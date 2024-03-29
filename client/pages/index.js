bd9e55ba4c7905646116d70e39893db13f8b96c4
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2024-03-29 04:34:13 +0200

    show tickets list in root directory page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,41 @@
 import React from "react";
+import Link from "next/link";
 
-const LandingPage = ({ currentUser }) => {
-  return <h1>You are {!currentUser && "Not"} Signed in</h1>;
+const LandingPage = ({ currentUser, tickets }) => {
+  return (
+    <div>
+      <h1>Tickets List</h1>
+      <table className="table text-center table-bordered">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Show</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tickets.map((ticket, i) => (
+            <tr key={ticket.id}>
+              <td>{i + 1}</td>
+              <td>{ticket.title}</td>
+              <td>{ticket.price}</td>
+              <td>
+                <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+                  Show Ticket
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  return {};
+  const { data } = await client.get("/api/tickets");
+  return { tickets: data };
 };
 
 export default LandingPage;
